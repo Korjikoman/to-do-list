1cd419170554adf098e612c8419e44a1c03d30fd
Stasyan <showmasniok@gmail.com>
2024-05-10 16:52:14 +0700

    My to-do app first commit

---
+++ todo_list/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Task(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.CharField(max_length=50)
+    description = models.TextField(max_length=150)
+    created_date = models.DateField(default=timezone.now)
+    task_done = models.BooleanField(default=False)
+    
+    
+    def create(self):
+        self.created_date = timezone.now()
+        self.save()
+    
+    def __str__(self) -> str:
+        return self.text
