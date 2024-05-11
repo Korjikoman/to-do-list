082adbd45b2bf3a5b11c03ea27c6e874c5098aa9
Stasyan <showmasniok@gmail.com>
2024-05-11 11:20:22 +0700

    Added some basic HTML just to see that it works correct

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,8 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def task_list(request):
+    return render(request, 'todo_list/task_list.html', {})
+
+
